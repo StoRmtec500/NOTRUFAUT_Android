056b9d6c7380929987bd347652e8d5777a3dab64
Martin <martin@kuenz.co.at>
2013-02-27 21:52:04 +0100

    ende

--- gen/at/co/kuenz/notrufaut/R.java
+++ gen/at/co/kuenz/notrufaut/R.java
@@ -23,6 +23,7 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int main8dp=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -31,7 +32,8 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090007;
+        public static final int imageButton1=0x7f090006;
         public static final int textView1=0x7f090002;
         public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090003;
